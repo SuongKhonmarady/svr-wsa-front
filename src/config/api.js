37d7f918fa4d936577990f704c04ee6ef6114f30
Suong Khonmarady <maradybor@gmail.com>
2025-08-28 10:21:11 +0700

    update api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,10 +1,10 @@
 // API Configuration
 const API_CONFIG = {
   // Base API URL
-  BASE_URL: 'http://44.220.150.9/api',
+  BASE_URL: 'https://api-scholar.site/api',
   
   // Storage URL for images
-  STORAGE_URL: 'http://44.220.150.9/storage',
+  STORAGE_URL: 'https://api-scholar.site/storage',
 
   // Request timeout in milliseconds
   TIMEOUT: 30000, // Increased to 30 seconds

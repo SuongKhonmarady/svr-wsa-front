b912c4e1619f20d689bde877f396558e770f0677
Suong Khonmarady <maradybor@gmail.com>
2025-08-29 11:26:50 +0700

    fix background image

--- src/pages/users/home/home.jsx
+++ src/pages/users/home/home.jsx
@@ -5,16 +5,17 @@ import NewsSection from './Components/NewsSection'
 
 function Home() {
     return (
-        <div 
-            className="min-h-screen bg-cover bg-center bg-no-repeat bg-[url('/image/fd78805f8ce862135726b6fc7f51aafc.jpg')] bg-fixed"
-            // style={{
-            //     backgroundImage: "url('/image/fd78805f8ce862135726b6fc7f51aafc.jpg')",
-            // }}
-        >
-            <HeroSection />
-            <ServicesOverview />
-            <ServiceRequestSection />
-            <NewsSection />
+        <div className="min-h-screen relative">
+            {/* Fixed background */}
+            <div className="fixed inset-0 -z-10 bg-cover bg-center bg-no-repeat bg-[url('/image/fd78805f8ce862135726b6fc7f51aafc.jpg')]" />
+            
+            {/* Content */}
+            <div className="relative z-10">
+                <HeroSection />
+                <ServicesOverview />
+                <ServiceRequestSection />
+                <NewsSection />
+            </div>
         </div>
     )
 }

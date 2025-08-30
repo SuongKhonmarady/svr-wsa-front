1fc603924f00a196773ab63dc96365da2660e591
Suong Khonmarady <maradybor@gmail.com>
2025-08-30 14:18:19 +0700

    Refactor Home component to simplify background handling by removing the BackgroundWrapper and directly applying responsive styles, enhancing readability and maintainability.

--- src/pages/users/home/home.jsx
+++ src/pages/users/home/home.jsx
@@ -5,38 +5,21 @@ import NewsSection from './Components/NewsSection'
 import { useState, useEffect } from 'react'
 
 function Home() {
-    const BackgroundWrapper = ({ children }) => {
-        const [isMobile, setIsMobile] = useState(false);
-
-        useEffect(() => {
-            const checkMobile = () => {
-                setIsMobile(/iPhone|iPad|iPod|Android/i.test(navigator.userAgent));
-            };
-            checkMobile();
-            window.addEventListener('resize', checkMobile);
-            return () => window.removeEventListener('resize', checkMobile);
-        }, []);
-
-        const backgroundClass = isMobile
-            ? "min-h-screen bg-cover bg-center bg-no-repeat bg-[url('/image/fd78805f8ce862135726b6fc7f51aafc.jpg')]"
-            : "min-h-screen bg-fixed bg-cover bg-center bg-no-repeat bg-[url('/image/fd78805f8ce862135726b6fc7f51aafc.jpg')]";
-
-        return (
-            <div className={backgroundClass}>
-                <div className="relative z-10">
-                    {children}
-                </div>
-            </div>
-        );
-    };
-
     return (
-        <BackgroundWrapper>
-            <HeroSection />
-            <ServicesOverview />
-            <ServiceRequestSection />
-            <NewsSection />
-        </BackgroundWrapper>
+        <div className="relative min-h-screen">
+            {/* Background for mobile/iOS */}
+            <div
+                className="fixed inset-0 -z-10 bg-cover bg-center bg-no-repeat bg-[url('/image/fd78805f8ce862135726b6fc7f51aafc.jpg')]"
+                style={{ height: '100%', minHeight: '100vh' }}
+            />
+            {/* Content wrapper */}
+            <div className="relative z-10 min-h-screen">
+                <HeroSection />
+                <ServicesOverview />
+                <ServiceRequestSection />
+                <NewsSection />
+            </div>
+        </div>
     )
 }
 
